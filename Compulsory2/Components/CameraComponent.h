fe2c7015906ccd130b33ba70cd2ad57f37375f0d
Anders <andersaugust@protonmail.com>
2024-10-09 13:57:03 +0200

    ECS is crazy

---
+++ Compulsory2/Components/CameraComponent.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "glm/vec3.hpp"
+
+struct CameraComponent {
+    glm::vec3 cameraFront;
+    glm::vec3 cameraUp;
+    float yaw;
+    float pitch;
+};
---
+++ Compulsory2/Components/TransformComponent.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "glm/vec3.hpp"
+
+struct TransformComponent {
+    glm::vec3 position;
+    glm::vec3 scale;
+    glm::vec3 rotation;
+    //TODO: Consider making separate component or smth
+    glm::vec3 velocity;
+};
