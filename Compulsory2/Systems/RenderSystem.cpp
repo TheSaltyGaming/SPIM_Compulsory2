1e353367a2c394d439fd2218f5bd39b893896dea
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2024-10-14 20:18:06 +0200

    PrepForBigChange

--- Compulsory2/Systems/RenderSystem.cpp
+++ Compulsory2/Systems/RenderSystem.cpp
@@ -1,1 +1,40 @@
 ﻿#include "RenderSystem.h"
+
+#include "../ECS/ComponentManager.h"
+#include "../Mesh/Mesh.h"
+#include "glad/glad.h"
+#include "glm/fwd.hpp"
+
+void RenderSystem::Update(std::vector<Entity>& entities, float deltaTime)
+{
+	for (auto& entity : entities)
+	{
+		if (mComponentManager.HasComponent<MeshComponent>(entity.GetId()) && mComponentManager.HasComponent<TransformComponent>(entity.GetId()))
+		{
+			//TODO: FIX THIS
+			// auto& meshComponent = mComponentManager.GetComponent<MeshComponent>(entity.GetId());
+			//
+			// auto& transformComponent = mComponentManager.GetComponent<TransformComponent>(entity.GetId());
+		}
+	}
+}
+
+void RenderSystem::RenderEntity(MeshComponent& meshComponent, TransformComponent& transformComponent)
+{
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::translate(modelMatrix, transformComponent.position);
+	modelMatrix = glm::rotate(modelMatrix, glm::radians(transformComponent.rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	modelMatrix = glm::rotate(modelMatrix, glm::radians(transformComponent.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	modelMatrix = glm::rotate(modelMatrix, glm::radians(transformComponent.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+	modelMatrix = glm::scale(modelMatrix, transformComponent.scale);
+
+	// glUSeProgram(mShaderProgram);
+	int modelLoc = glGetUniformLocation(mShaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+
+	if (meshComponent.mesh)
+	{
+		// meshComponent.mesh->Draw();
+	}
+
+}
--- Compulsory2/Systems/RenderSystem.h
+++ Compulsory2/Systems/RenderSystem.h
@@ -1,7 +1,21 @@
 ﻿#pragma once
+#include "System.h"
+#include "../Components/MeshComponent.h"
+#include "../Components/TransformComponent.h"
+#include <glm/matrix.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-class RenderSystem
+class RenderSystem : public System
 {
 public:
-    
+
+    RenderSystem(ComponentManager& componentManager, unsigned int ShaderProgram) : mComponentManager(componentManager), mShaderProgram(ShaderProgram){};
+
+    void Update(std::vector<Entity>& entities, float deltaTime) override;
+
+    void RenderEntity(MeshComponent& meshComponent, TransformComponent& transformComponent);
+
+private:
+    ComponentManager& mComponentManager;
+    unsigned int mShaderProgram;
 };
